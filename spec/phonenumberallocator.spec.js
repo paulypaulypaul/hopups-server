880183fb60f3e4ab62cc922cf18e5aadd5bbf850
paul <harlandpauly@gmail.com>
2016-03-14 21:41:47 +0000

    adds tests

--- spec/phonenumberallocator.spec.js
+++ spec/phonenumberallocator.spec.js
@@ -11,6 +11,7 @@ describe("Rules engine tests", function() {
   var connection;
   var site;
   var user;
+  var user2;
   beforeEach(function(done) {
     connection = mongoose.connect('mongodb://localhost/hopups-test', function(){
       Site.create([
@@ -23,10 +24,11 @@ describe("Rules engine tests", function() {
         site = sites[0];
 
         SiteUser.create([
+          {"siteId":site._id},
           {"siteId":site._id}
         ], function(err, siteusers){
-          console.log('$$$$$$$$$', siteusers)
           user = siteusers[0];
+          user2 = siteusers[1];
           done();
         });
       });
@@ -35,7 +37,7 @@ describe("Rules engine tests", function() {
 
   afterEach(function(done) {
     console.log('after each');
-  //  connection.connection.db.dropDatabase();
+    connection.connection.db.dropDatabase();
     mongoose.connection.close(function () {
       console.log('Mongoose disconnected on app termination');
       done();
@@ -43,14 +45,30 @@ describe("Rules engine tests", function() {
   });
 
   it("should allocate phone number", function(done) {
+    phoneNumberAllocator.allocatePhoneNumber(user).then(function(user){
+      expect(user._doc.phoneNo).toEqual('111');
 
-      phoneNumberAllocator.allocatePhoneNumber(user).then(function(user){
+      //the site has had allocated phoen numbers updated
+      Site.findOne({_id: user.siteId}).then(function(site){
+        expect(site.allocatedPhoneNumbers).toEqual(['111']);
 
-        console.log('@@@@@@@@@@@@@@@@@@@@@@@--', user, user.phoneNo);
-        done();
+        //as the first allocation is still not times out we expect the ame back
+        phoneNumberAllocator.allocatePhoneNumber(user).then(function(user){
+          expect(user._doc.phoneNo).toEqual('111');
 
+          phoneNumberAllocator.allocatePhoneNumber(user2).then(function(user2){
+            expect(user2._doc.phoneNo).toEqual('222');
+
+            //the site has had allocated phoen numbers updated
+            Site.findOne({_id: user.siteId}).then(function(site){
+              expect(site.allocatedPhoneNumbers).toEqual(['111','222']);
+
+              done();
+            });
+          });
+        });
       });
-
+    });
   });
 
 });
