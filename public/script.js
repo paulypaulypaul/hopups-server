9f39b99da6259a07fbec1802c2a4eb8d6f15d0a9
paul <harlandpauly@gmail.com>
2016-03-28 23:31:54 +0100

    move around function delaire

--- public/script.js
+++ public/script.js
@@ -1,31 +1,3 @@
-//load jquery if its not here
-if (typeof jQuery == 'undefined') {
-
-	function getScript(url, success) {
-		var script     = document.createElement('script');
-		script.src = url;
-		var head = document.getElementsByTagName('head')[0],
-		done = false;
-		// Attach handlers for all browsers
-		script.onload = script.onreadystatechange = function() {
-			if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-			done = true;
-				// callback function provided as param
-				success();
-				script.onload = script.onreadystatechange = null;
-				head.removeChild(script);
-			};
-		};
-		head.appendChild(script);
-	};
-
-	getScript('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', function() {
-    start();
-  });
-} else {
-  start();
-};
-
 var start = function(){
 
     //nanomodal  load
@@ -400,3 +372,32 @@ var start = function(){
   });
 
 }
+
+
+//load jquery if its not here
+if (typeof jQuery == 'undefined') {
+
+	function getScript(url, success) {
+		var script     = document.createElement('script');
+		script.src = url;
+		var head = document.getElementsByTagName('head')[0],
+		done = false;
+		// Attach handlers for all browsers
+		script.onload = script.onreadystatechange = function() {
+			if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+			done = true;
+				// callback function provided as param
+				success();
+				script.onload = script.onreadystatechange = null;
+				head.removeChild(script);
+			};
+		};
+		head.appendChild(script);
+	};
+
+	getScript('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', function() {
+    start();
+  });
+} else {
+  start();
+};
