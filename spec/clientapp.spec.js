e79f790ce312eb335507d02da668461fe4fc5446
paul <harlandpauly@gmail.com>
2016-04-17 16:33:51 +0100

    holder for client app tests

---
+++ spec/clientapp.spec.js
@@ -0,0 +1,21 @@
+var clientApp = require('../public/script.js');
+
+
+describe("Test client app", function() {
+
+  beforeEach(function(done) {
+    done();
+  });
+
+  afterEach(function(done) {
+    done();
+  });
+
+  it("It should initialise", function(done) {
+
+    clientApp
+
+
+  });
+
+});
